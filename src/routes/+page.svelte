54b3b3a7d8a15aa21c1c528ea10e50796982568d
Mayumi <mayumiwm@gmail.com>
2024-11-08 10:47:17 -0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<div class="flex justify-center items-center h-screen">
+    <div class="container bg-gray-800 p-10 rounded-lg">
+        <a href="/playlist" class="bg-purple-500 px-6 py-3 rounded-full font-semibold hover:bg-emerald-400">
+            PLAYLISTS
+        </a>
+
+        <div class="flex flex-row justify-between mt-10 mb-2 items-center">
+            <p class="text-2xl font-semibold">
+                SONGS
+            </p>
+            <a href="/playlist" class="bg-purple-500 px-6 py-3 rounded-full font-semibold hover:bg-emerald-400">
+                ADD SONG
+            </a>
+        </div>
+
+        <table class="table-auto w-full">
+            <thead class="text-left border-b-2 border-slate-400">
+              <tr>
+                <th>Song</th>
+                <th>Artist</th>
+                <th>Year</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="border-b border-slate-400">
+                <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                <td>Malcolm Lockyer</td>
+                <td>1961</td>
+                <td class="text-right"><a href="#" class="bg-purple-500 px-6 py-3 rounded-full font-semibold hover:bg-emerald-400">
+                    EDIT
+                </a></td>
+              </tr>
+              <tr class="border-b border-slate-400">
+                <td>Witchy Woman</td>
+                <td>The Eagles</td>
+                <td>1972</td>
+              </tr>
+              <tr class="border-b border-slate-400">
+                <td>Shining Star</td>
+                <td>Earth, Wind, and Fire</td>
+                <td>1975</td>
+              </tr>
+            </tbody>
+          </table>
+    </div>
+</div>
---
+++ src/routes/playlist/+page.svelte
@@ -0,0 +1,7 @@
+<div class="flex justify-center items-center h-screen">
+    <div class="container bg-gray-800 p-10 rounded-lg">
+        <a href="/" class="bg-purple-500 px-6 py-3 rounded-full">
+            Voltar
+        </a>
+    </div>
+</div>
